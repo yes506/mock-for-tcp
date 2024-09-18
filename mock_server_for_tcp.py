8b5b0af001a17e03594e63ae00c1cbef353921be
김동현 <donghyeon@DongHyeon-MacBookPro-2.local>
2024-09-18 23:54:54 +0900

    first commit

---
+++ mock_server_for_tcp.py
@@ -0,0 +1,28 @@
+import socket
+
+
+def server_for_tcp():
+    # get hostname
+    host = socket.gethostname()
+    port = 30105
+
+    server_socket = socket.socket()
+    server_socket.bind((host, port))
+
+    server_socket.listen(2)
+    conn, address = server_socket.accept()
+    print("Connection from: " + str(address))
+
+    while True:
+        data = conn.recv(1024).decode()
+        if not data:
+            break
+        print("from connected client: " + str(data))
+        response = "Hello from server"
+        conn.send(response.encode())
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    server_for_tcp()
