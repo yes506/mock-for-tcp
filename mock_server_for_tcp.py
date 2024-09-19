086fb87780539201e73d90438748e9385031f145
김동현 <donghyeon@DongHyeon-MacBookPro-2.local>
2024-09-19 10:49:34 +0900

    fix

--- mock_server_for_tcp.py
+++ mock_server_for_tcp.py
@@ -1,17 +1,20 @@
-import asyncio
-import time
-import websockets
-import datetime
-import multiprocessing as mp
+import socket
 
 
-if __name__ == "__main__":
-    async def handler(websocket, path):
-        while True:
-            now = datetime.datetime.utcnow().isoformat() + "Z"
-            await websocket.send(f"From Mock TCP Server: {now}")
-            time.sleep(0.1)
+IP = "10.241.238.4"
+PORT = 30105
+SIZE = 1024
+ADDRESS = (IP, PORT)
 
-    start_server = websockets.serve(handler, host="10.241.238.4", port=30105)
-    asyncio.get_event_loop().run_until_complete(start_server)
-    asyncio.get_event_loop().run_forever()
+# configure the server
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+    server_socket.bind(ADDRESS)
+    server_socket.listen()
+
+    while True:
+        client_socket, client_address = server_socket.accept() # 수신 대기
+        message = client_socket.recv(SIZE)
+        if message:
+            print(f"From {client_address} received: {message}")
+            client_socket.sendall("Hello, client. I'm a server.".encode())
+            
