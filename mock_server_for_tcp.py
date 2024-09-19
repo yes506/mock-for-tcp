a3bed2409dd7c6ff4b9bd6f62af4f08a45f920cf
김동현 <donghyeon@DongHyeon-MacBookPro-2.local>
2024-09-19 10:58:23 +0900

    fix

--- mock_server_for_tcp.py
+++ mock_server_for_tcp.py
@@ -7,14 +7,18 @@ SIZE = 1024
 ADDRESS = (IP, PORT)
 
 # configure the server
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-    server_socket.bind(ADDRESS)
-    server_socket.listen()
+def run_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind(ADDRESS)
+        server_socket.listen()
 
-    while True:
-        client_socket, client_address = server_socket.accept() # 수신 대기
-        message = client_socket.recv(SIZE)
-        if message:
-            print(f"From {client_address} received: {message}")
-            client_socket.sendall("Hello, client. I'm a server.".encode())
-            
+        while True:
+            client_socket, client_address = server_socket.accept() # 수신 대기
+            message = client_socket.recv(SIZE)
+            if message:
+                print(f"From {client_address} received: {message}")
+                client_socket.sendall("Hello, client. I'm a server.".encode())
+
+
+if __name__ == "__main__":
+    run_server()
